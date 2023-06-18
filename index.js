e2549ba1d4ea2a330fcdb520b2248806178ff6ca
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-06-18 23:34:28 +0300

    not fully done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+  let newCollection = [];
+  const lowerCaseName = name.toLowerCase();
+
+  for (const item of drivers) {
+    if (item.toLowerCase() === lowerCaseName) {
+      newCollection.push(item);
+    }
+  }
+
+  return newCollection;
+}
+
+function  fuzzyMatch(driver,name){
+    let newCollection = [];
+
+  for (const item of drivers) {
+    if (item.includes(name) ) {
+      newCollection.push(item);
+    }
+    else if (item === name){
+        newCollection = 0
+    }
+}
+
+  return newCollection;
+
+}
